4afd064190a50ed689d92577f857f0cf10b77c78
Koryah1100 <Kanyada.yu.67@ubu.ac.th>
2025-01-24 14:04:05 +0700

    สร้างคลาสครั้งแรกบน Githab repo5555

---
+++ app.py
@@ -0,0 +1,23 @@
+class Student(object):
+  student_count=0
+  
+  def __new__(self):
+  print('student_new')
+  def __init__(self):
+  printt('Student,__init__')
+  self.gpa =4.00
+  def student_hard(self):
+  print('Sir,yes sir.')
+
+
+
+
+
+
+
+
+
+  
+
+
+
